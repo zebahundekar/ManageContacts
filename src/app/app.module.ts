1ca2080e4643a32f027c6cd60a9913d21fe0dc19
Zeba <zeba.sweetgirl@gmail.com>
2018-07-16 00:21:57 +0530

    Manage Contacts

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactsComponent
+  ],
+  imports: [
+    BrowserModule,
+	FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css']
+})
+
+export class ContactsComponent implements OnInit {
+
+  contacts: Array<ContactInfo>;
+  contact: ContactInfo;
+  addNewContact: Boolean;
+  editContact: Boolean;
+  
+  constructor() {
+	  this.contacts = [{FirstName: "Sarah", LastName: "Kane", Id: 1, Email:"kanesarah@xyz.com", PhoneNumber: 1234567890, IsActive: true},
+		{FirstName: "John", LastName: "Doe", Id: 2, Email:"johndoe@abc.com", PhoneNumber: 9977886655, IsActive: true}
+	  ];
+	  this.contact = {};
+	  this.addNewContact = false;
+	  this.editContact = false;
+  }
+
+  ShowEditContactForm(contact) {
+	  this.editContact = true;
+	  this.addNewContact = false;	  
+	  this.contact = contact;
+  }
+
+  ShowAddContactForm() {
+	  this.contact = {FirstName: "", LastName: ""};
+	  this.addNewContact = true;
+	  this.editContact = false;
+  }
+  
+  AddContact(contact) {
+	  contact.IsActive = true;
+	  this.contacts.push(contact);
+	  this.addNewContact = false;	  
+	  this.contact = {};
+  }
+  
+  DeleteContact(contact) {
+	  for(let index = 0; index < this.contacts.length; index++) {
+		  if(this.contacts[index].Id == contact.Id)
+			  this.contacts[index].IsActive = false;
+	  }
+  }
+  
+  SaveContact(contact) {
+	  for(let index = 0; index < this.contacts.length; index++) {
+		  if(this.contacts[index].Id == contact.Id)
+			  this.contacts[index] = contact;
+	  }
+	  this.editContact = false;	
+	  this.contact = {};
+  }  
+}
+
+export module ContactInfo {
+	FirstName: String;
+	LastName: String;
+	Email: String;
+	PhoneNumber: Number;
+	Id: Number;
+	IsActive: Boolean;
+}
